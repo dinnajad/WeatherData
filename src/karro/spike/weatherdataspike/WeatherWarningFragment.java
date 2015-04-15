bffdbe12a5472daa2a15f842c616694958a5ea2b
dinnajad <dinnajad@gmail.com>
2015-04-15 11:38:14 +0200

    Skapat custom adapter för vädervarningarna. går absolut att fila på utseendet men det funkar

--- src/karro/spike/weatherdataspike/WeatherWarningFragment.java
+++ src/karro/spike/weatherdataspike/WeatherWarningFragment.java
@@ -4,12 +4,17 @@
 package karro.spike.weatherdataspike;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import karro.spike.weatherdata.R;
 import karro.spike.weatherdataspike.model.WeatherWarning;
 import karro.spike.weatherdataspike.model.WeatherWarningKeeper;
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 /**
  * @author Karro
@@ -36,8 +41,9 @@ public class WeatherWarningFragment extends ListFragment{
 		warnings = keeper.getWarnings();// varför är den tom?
 		//skapa adapter
 		//TODO skapa custom adapter med bättre vy
-		ArrayAdapter<WeatherWarning> adapter = new ArrayAdapter<WeatherWarning>(getActivity().getApplicationContext(), 
-				android.R.layout.simple_list_item_1, warnings);
+//		ArrayAdapter<WeatherWarning> adapter = new ArrayAdapter<WeatherWarning>(getActivity().getApplicationContext(), 
+//				android.R.layout.simple_list_item_1, warnings);
+		WeatherWarningAdapter adapter = new WeatherWarningAdapter(warnings);
 		setListAdapter(adapter);
 
 	}
@@ -50,4 +56,33 @@ public class WeatherWarningFragment extends ListFragment{
 		View v= inflater.inflate(R.layout.fragment_alarm_list, container,false);
 		return v;
 		}*/
+	private class WeatherWarningAdapter extends ArrayAdapter<WeatherWarning>{
+
+		public WeatherWarningAdapter(List<WeatherWarning> warnings) {
+			super(getActivity(), 0, warnings);
+			
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent){
+			if(convertView == null){
+				convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_weather_warnings, null);
+			}
+			WeatherWarning warning = getItem(position);
+			
+			TextView messageTextView = (TextView)convertView.findViewById(R.id.message_textView_li);
+			messageTextView.setText(warning.getMessage());
+			
+			TextView alarmTextView = (TextView)convertView.findViewById(R.id.alarm_textView_li);
+			alarmTextView.setText(warning.getAlarm().toString());
+			
+			TextView time = (TextView)convertView.findViewById(R.id.time_textView_li);
+			time.setText(warning.getOneDayWeatherData().getDayString());
+			
+			
+			return convertView;
+			
+		}
+		
+	}
 }
